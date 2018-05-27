faeec1c1a7a52d3e861bb147d6cdac560855e308
Albert Cansado Solà <albert.cansado@gmail.com>
2018-05-27 16:41:03 +0200

    add adminField property
    - adminField
    - CS

--- lib/class.LISEFielddefBase.php
+++ lib/class.LISEFielddefBase.php
@@ -66,16 +66,16 @@ abstract class LISEFielddefBase implements ArrayAccess
   #---------------------
   # Constants
   #---------------------
-  
+
   const TYPE_STRING = "string";
   const TYPE_ARRAY = "array";
   const TYPE_OBJECT = "object";
   const TYPE_TIMESTAMP = "timestamp";
-  
+
   #---------------------
   # Variables
   #---------------------
-  
+
   private $id;
   private $name;
   private $alias;
@@ -96,14 +96,20 @@ abstract class LISEFielddefBase implements ArrayAccess
   private $template;
   private $view;
   private $hastemplate;
-  
+
   protected $event_handler;
-  
+
+  /**
+   * True if field is used only on admin section
+   * @var bool
+   */
+  protected $adminField = false;
+
   #---------------------
   # Magic methods
   #---------------------
-  
-  public function __construct(&$db_info, $caller_object = NULL)
+
+  public function __construct(&$db_info, $caller_object = null)
   {
     $this->id           = -1;
     $this->name         = '';
@@ -117,54 +123,53 @@ abstract class LISEFielddefBase implements ArrayAccess
     $this->friendlytype = $db_info->type;
     $this->value        = new LISEFielddefValue;
     $this->position     = -1;
-    $this->required     = 0;
+    $this->required     = false;
     $this->options      = array();
-    $this->caller       = NULL;
+    $this->caller       = null;
     $this->item_id      = -1;
-    $this->hastemplate  = FALSE;
-    $this->template     = NULL;
+    $this->hastemplate  = false;
+    $this->template     = null;
     $this->_get_default_template();
-    
-    if($caller_object instanceof CMSModule)
-    {
+
+    if ($caller_object instanceof CMSModule) {
       $this->caller = $caller_object->GetName();
     }
   }
-  
+
   public function __get($key)
   {
     return $this->_overwrite_constants($key);
   }
-  
+
   public function __toString()
   {
     return (string)$this->value;
   }
-  
+
   public function __call($name, $args)
   {
-    return FALSE;
+    return false;
   }
-  
+
   #---------------------
   # Array methods
   #---------------------
-  
+
   public function offsetGet($offset)
   {
     return $this->_overwrite_constants($offset);
   }
-  
+
   public function offsetSet($offset, $value){ }
-  
+
   public function offsetExists($offset){ }
-  
+
   public function offsetUnset($offset){ }
-  
+
   #---------------------
   # Private methods
   #---------------------
-  
+
   /**
    * limits the property access to:
    * name, value, type, alias, template and view
@@ -180,227 +185,227 @@ abstract class LISEFielddefBase implements ArrayAccess
     {
       case 'name':
         return $this->GetName();
-      
+
       case 'value':
         return $this->GetValue(self::TYPE_STRING);
-      
+
       case 'type':
         return $this->GetType();
-      
+
       case 'alias':
         return $this->GetAlias();
-      
+
       case 'template':
         return $this->GetTemplate();
-      
+
       case 'view':
         return $this->FrontEndRender();
-      
+
       default:
         return NULL;
     }
   }
-  
+
   private function _get_default_template()
   {
     $fn = $this->GetPath() . DIRECTORY_SEPARATOR . 'output.' . $this->GetType() . '.tpl';
-    
+
     if(is_readable($fn))
     {
       $this->template    = @file_get_contents($fn);
       $this->hastemplate = TRUE;
     }
-    
+
     return $this->hastemplate;
   }
-  
+
   #---------------------
   # get/set methods
   #---------------------
-  
+
   public final function GetId()
   {
     return $this->id;
   }
-  
+
   public final function SetId($value)
   {
     $this->id = $value;
   }
-  
+
   public final function GetName()
   {
     return $this->name;
   }
-  
+
   public final function SetName($value)
   {
     $this->name = $value;
   }
-  
+
   public final function GetAlias()
   {
     return $this->alias;
   }
-  
+
   public final function SetAlias($value)
   {
     $this->alias = $value;
   }
-  
+
   public final function GetDesc()
   {
     return $this->description;
   }
-  
+
   public final function SetDesc($value)
   {
     $this->description = $value;
   }
-  
+
   public final function GetType()
   {
     return $this->type;
   }
-  
+
   public final function GetFriendlyType()
   {
     return $this->friendlytype;
   }
-  
+
   public final function SetFriendlyType($value)
   {
     $this->friendlytype = $value;
   }
-  
+
   public final function HasValue()
   {
     if($this->GetValue(self::TYPE_STRING) !== '')
     {
       return TRUE;
     }
-    
+
     return FALSE;
   }
-  
+
   public final function GetValue($type = self::TYPE_OBJECT)
   {
     $type = strtolower($type);
-    
+
     switch($type)
     {
       case self::TYPE_STRING:
         return (string)$this->value;
-      
+
       case self::TYPE_ARRAY:
         return (array)$this->value;
-      
+
       case self::TYPE_TIMESTAMP:
         return strtotime((string)$this->value);
-      
+
       default:
         return $this->value;
     }
   }
-  
+
   public final function SetValue($value = array())
   {
     $this->value = new LISEFielddefValue($value);
   }
-  
+
   public final function GetOriginator()
   {
     return $this->originator;
   }
-  
+
   public final function IsActive()
   {
     return $this->active ? TRUE : FALSE;
   }
-  
+
   public final function IsDisabled()
   {
     return $this->disabled ? TRUE : FALSE;
   }
-  
+
   // deprecated
   public final function GetActive()
   {
     return $this->active;
   }
-  
+
   public final function GetPosition()
   {
     return $this->position;
   }
-  
+
   public final function SetPosition($value)
   {
     $this->position = $value;
   }
-  
+
   public final function GetPath()
   {
     return $this->path;
   }
-  
+
   public final function GetURLPath()
   {
     $config = cmsms()->GetConfig();
-    
+
     $url = substr($this->GetPath(), strlen($config['root_path']));
     $url = str_replace(DIRECTORY_SEPARATOR, '/', $url);
     $url = $config->smart_root_url() . $url;
-    
+
     return $url;
   }
-  
+
   public final function IsRequired()
   {
-    return $this->required ? TRUE : FALSE;
+    return (bool)$this->required;
   }
-  
+
   public final function GetRequired()
   {
-    return $this->required;
+    return (bool)$this->required;
   }
-  
+
   public final function SetRequired($value)
   {
-    $this->required = $value;
+    $this->required = (bool)$value;
   }
-  
+
   public final function GetItemId()
   {
     return $this->GetParentItem()->item_id;
   }
-  
+
   public final function GetItemAlias()
   {
     return $this->GetParentItem()->alias;
   }
-  
+
   // deprecated
   public final function SetItemId($value)
   {
     $this->item_id = $value;
   }
-  
+
   public final function GetParentArray()
   {
     if(isset($this->parent_array))
     {
       return $this->parent_array;
     }
-    
+
     return FALSE;
   }
-  
+
   public final function SetParentArray(LISEFielddefArray &$obj)
   {
     $this->parent_array = $obj;
   }
-  
+
   public final function GetParentItem()
   {
     if($this->GetParentArray())
@@ -410,59 +415,78 @@ abstract class LISEFielddefBase implements ArrayAccess
         return $this->GetParentArray()->GetParentItem();
       }
     }
-    
+
     return FALSE;
   }
-  
+
   public final function SetTemplate($content)
   {
     $this->template = $content;
   }
-  
+
   public final function GetTemplate()
   {
     return $this->template;
   }
-  
+
   public final function HasTemplate()
   {
     return $this->hastemplate;
   }
-  
+
+  public function isAdminField()
+  {
+    return (bool)$this->isAdminField;
+  }
+
+  public function enableAdminField()
+  {
+    $this->isAdminField = true;
+
+    return $this;
+  }
+
+  public function disableAdminField()
+  {
+    $this->isAdminField = false;
+
+    return $this;
+  }
+
   #---------------------
   # Option methods
   #---------------------
-  
+
   public final function GetOptionValues()
   {
     return $this->options;
   }
-  
+
   public final function GetOptionValue($key, $default = '')
   {
     return isset($this->options[$key]) ? $this->options[$key] : $default;
   }
-  
+
   public final function SetOptionValue($key, $value)
   {
     $this->options[$key] = $value;
   }
-  
+
   #---------------------
   # Overwrite methods
   #---------------------
-  
+
   static public function GetModuleDeps()
   {
     return NULL;
   }
-  
+
   public function GetHeaderHTML()
   {
     $ret = NULL;
-    
+
     $fn = $this->GetPath() . DIRECTORY_SEPARATOR . 'header.' . $this->GetType() . '.tpl';
-    
+
     if(is_readable($fn))
     {
       $ret    = @file_get_contents($fn);
@@ -470,53 +494,53 @@ abstract class LISEFielddefBase implements ArrayAccess
       $smarty->assign('fielddefdir', $this->GetURLPath());
       $ret = $smarty->fetch('eval:' . $ret);
     }
-    
+
     return $ret;
   }
-  
+
   public function IsUnique()
   {
     return FALSE;
   }
-  
+
   public function RenderForAdminListing($id, $returnid)
   {
     $rn = 'list.' . $this->GetType() . '.tpl';
     $fn = $this->GetPath() . DIRECTORY_SEPARATOR . $rn;
-    
-    
+
+
     if( is_readable($fn) )
     {
       $smarty = cmsms()->GetSmarty();
       $smarty->assign('actionid', $id);
       $smarty->assign('returnid', $returnid);
-  
+
       return $smarty->fetch('lisetemplate:fielddefs;' . $this->GetOriginator() . ';'  . $rn);
     }
-    
+
     return $this->GetValue(self::TYPE_STRING);
   }
-  
+
   public function RenderForEdit($id, $returnid)
   {
     $rn = 'admin.' . $this->GetType() . '.tpl';
     $fn = $this->GetPath() . DIRECTORY_SEPARATOR . $rn;
-    
+
     if( is_readable($fn) )
     {
       $smarty = cmsms()->GetSmarty();
       $smarty->assign('actionid', $id);
       $smarty->assign('returnid', $returnid);
       return $smarty->fetch('lisetemplate:fielddefs;' . $this->GetOriginator() . ';'  . $rn);}
-    
+
     return '';
   }
-  
+
   public function RenderInput($id, $returnid)
   {
     $rn = 'input.' . $this->GetType() . '.tpl';
     $fn = $this->GetPath() . DIRECTORY_SEPARATOR . $rn;
-    
+
     if( is_readable($fn) )
     {
       $smarty = cmsms()->GetSmarty();
@@ -524,10 +548,10 @@ abstract class LISEFielddefBase implements ArrayAccess
       $smarty->assign('returnid', $returnid);
       return $smarty->fetch('lisetemplate:fielddefs;' . $this->GetOriginator() . ';'  . $rn);
     }
-    
+
     return '';
   }
-  
+
   public function FrontEndRender($params = array())
   {
     if(empty($this->view) && $this->hastemplate)
@@ -544,10 +568,10 @@ abstract class LISEFielddefBase implements ArrayAccess
     {
       $this->view = '';
     }
-    
+
     return $this->view;
   }
-  
+
   /**
    * a shorter alias to FrontEndRender() method
    */
@@ -555,7 +579,7 @@ abstract class LISEFielddefBase implements ArrayAccess
   {
     return $this->FrontEndRender($params);
   }
-  
+
   public function Validate(&$errors)
   {
     if($this->GetValue(self::TYPE_STRING) == '' && $this->IsRequired())
@@ -563,28 +587,28 @@ abstract class LISEFielddefBase implements ArrayAccess
       $errors[] = $this->ModLang('required_field_empty') . ' (' . $this->GetName() . ')';
     }
   }
-  
+
   #---------------------
   # Event handler
   #---------------------
-  
+
   /************************************
    * NOTICE: Highly experimental
    ************************************/
-  
+
   // Should i make this final or not, lets leave it like this for now.
   public function EventHandler()
   {
     // Check if field has own event handler
     if(!isset($this->event_handler))
     {
-      
+
       $fn = $this->GetPath() . DIRECTORY_SEPARATOR . 'liseeh.' . $this->GetType() . '.php';
       if(is_readable($fn))
       {
-        
+
         require_once($fn);
-        
+
         $class = 'liseeh_' . $this->GetType();
         if(class_exists($class))
         {
@@ -592,34 +616,34 @@ abstract class LISEFielddefBase implements ArrayAccess
         }
       }
     }
-    
+
     // Ensure that we have default event handler
     if(!isset($this->event_handler))
     {
       $this->event_handler = new LISEEventHandlerBase($this);
     }
-    
+
     return $this->event_handler;
   }
-  
+
   #---------------------
   # Module methods
   #---------------------
-  
+
   public final function GetModuleInstance($caller = FALSE)
   {
     if($caller && !is_null($this->caller))
     {
       return cmsms()->GetModuleInstance($this->caller);
     }
-    
+
     return cmsms()->GetModuleInstance($this->originator);
   }
-  
+
   #---------------------
   # Lang methods
   #---------------------
-  
+
   public final function ModLang()
   {
     $mod = $this->GetModuleInstance();
@@ -629,10 +653,10 @@ abstract class LISEFielddefBase implements ArrayAccess
     }
     //throw new LISEException('Could not retrive module instance from originator!'); // <- Send own missing lang string instead of failure?
     $args = func_get_args();
-    
+
     return call_user_func_array(array($mod, 'lang'), $args);
   }
-  
+
 } // end of class
 
 /**
@@ -647,20 +671,18 @@ class LISEFielddefValue extends ArrayObject
   #---------------------
   # Magic methods
   #---------------------
-  
-  public function __construct($array = array())
+
+  public function __construct($array = [])
   {
-    foreach((array)$array as $key => $value)
-    {
-      
+    foreach((array)$array as $key => $value) {
       $this->offsetSet($key, $value);
     }
   }
-  
+
   public function __toString()
   {
     return (string)implode(LISE_VALUE_SEPARATOR, (array)$this);
   }
-  
+
 } // end of class
 ?>
